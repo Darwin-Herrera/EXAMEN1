b7a906bf16606c28b9f4fa43d0387f791c3d7066
Darwin Herrera <66148345+Darwin-Herrera@users.noreply.github.com>
2023-08-20 18:18:45 -0600

    Posteo de Postgres

---
+++ Postgres/DB_Gravity.sql
@@ -0,0 +1,28 @@
+--- ITEMS 1 ---
+SELECT b.title AS TITULO, a.author_name AS AUTOR, bl.language_name 
+FROM book b 
+LEFT JOIN book_author ba ON b.book_id = ba.book_id 
+INNER JOIN author a ON ba.author_id = a.author_id 
+LEFT JOIN book_language bl ON b.language_id = bl.language_id 
+ORDER BY b.title ASC;
+
+--- ITEMS 5 ---
+SELECT b.title AS TITULO, os.status_value AS ESTATUS
+FROM book b 
+LEFT JOIN order_line ol ON b.book_id = ol.line_id 
+LEFT JOIN cust_order co ON ol.order_id = co.order_id 
+LEFT JOIN order_history oh ON ol.order_id = oh.order_id 
+INNER JOIN order_status os ON oh.status_id = os.status_id 
+WHERE os.status_id = 5
+ORDER BY b.title ASC;
+
+--- ITEMS 8 ---
+SELECT COUNT(b.title) AS CANTIDAD_LIBROS, b.title AS TITULO, os.status_value AS ESTATUS
+FROM book b 
+LEFT JOIN order_line ol ON b.book_id = ol.line_id 
+LEFT JOIN cust_order co ON ol.order_id = co.order_id 
+LEFT JOIN order_history oh ON ol.order_id = oh.order_id 
+INNER JOIN order_status os ON oh.status_id = os.status_id 
+WHERE os.status_id = 4
+GROUP BY b.title, os.status_value
+ORDER BY b.title ASC;
---
+++ Postgres/DB_Universities.sql
@@ -0,0 +1,12 @@
+--- ITEMS 1 ---
+SELECT u.university_name AS UNIVERSIDAD, c.country_name AS UBICACIÓN
+FROM university u 
+INNER JOIN country c ON u.country_id = c.id;
+
+--- ITEMS 2 ---
+SELECT c.country_name AS PAIS, COUNT(u.id) AS CANTIDAD_UNIVERSIDADES 
+FROM country c 
+INNER JOIN university u ON c.id = u.country_id 
+GROUP BY c.country_name 
+ORDER BY c.country_name ASC;
+
